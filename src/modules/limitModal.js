5e529365aecafb1473d2b92820715e2cead1ead7
daffadevhosting <endsae83@gmail.com>
2025-06-19 13:59:29 +0700

    update

--- src/modules/limitModal.js
+++ src/modules/limitModal.js
@@ -1,9 +1,9 @@
 export function showLimitModal() {
   const modal = document.getElementById('loginModal');
-  if (modal) modal.classList.remove('hidden');
+  if (modal) modal.classList.remove('hide');
 }
 
 export function hideLimitModal() {
   const modal = document.getElementById('loginModal');
-  if (modal) modal.classList.add('hidden');
+  if (modal) modal.classList.add('hide');
 }
